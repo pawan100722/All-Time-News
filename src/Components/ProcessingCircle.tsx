c0702fc6a39b248ddd89863660d42e1eef6564a6
pawan100722 <pawan100722@gmail.com>
2025-04-21 15:41:26 +0530

    added processing

---
+++ src/Components/ProcessingCircle.tsx
@@ -0,0 +1,9 @@
+import '../Styles/ProcessingCircle.css';
+
+export const ProcessingCircle =()=>{
+  return (
+    <div className="processing-circle-container">
+      <div className="processing-circle"></div>
+    </div>
+  );
+}
--- src/Components/SearchNews.tsx
+++ src/Components/SearchNews.tsx
@@ -2,20 +2,24 @@ import { useEffect, useState } from 'react';
 import '../Styles/SearchNews.css';
 import { SearchIcon } from '../Icons/SearchIcon';
 import { SearchNewsPropDTO } from '../DTOS/PropsDTO';
+import { ProcessingCircle } from './ProcessingCircle';
 
 export const SearchNews=({setSearchKeywordProp}: SearchNewsPropDTO)=>{
 
   const [inputText, setInputText] = useState<string>('');
   const [timeoutId, setTimeoutId] = useState<number>(NaN);
+  const [isTyping, setIsTyping] = useState<boolean>(false)
 
   const handleInputChange=(eventParam: React.ChangeEvent<HTMLInputElement>)=>{
     setInputText(eventParam?.target?.value);
+    setIsTyping(true);
   }
   
   useEffect(()=>{
     clearTimeout(timeoutId);
     const id = setTimeout(()=>{
       setSearchKeywordProp(inputText);
+      setIsTyping(false);
     },1000);
     setTimeoutId(id);
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -26,5 +30,6 @@ export const SearchNews=({setSearchKeywordProp}: SearchNewsPropDTO)=>{
       <SearchIcon/>
     </div>
   <input type="text" className="search-input" placeholder='Search News Topics...' onChange={handleInputChange}/>
+  {isTyping?<ProcessingCircle/>:''}
   </div>
 }
